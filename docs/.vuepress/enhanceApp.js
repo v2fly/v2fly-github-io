c29aafeda7199ef8c922e292e614da0af0a2a367
Kid <44045911+kidonng@users.noreply.github.com>
2020-09-03 23:37:58 +0800

    Update enhanceApp.js

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,8 +1,12 @@
 export default () => {
-  // https://github.com/vuejs/vuepress/issues/2562
+  // https://github.com/vuejs/vuepress/issues/2562#issuecomment-686575512
   document.onreadystatechange = () => {
     if (document.readyState === 'complete') {
-      document.querySelector(decodeURIComponent(location.hash)).scrollIntoView()
+      const { hash } = location
+      const decoded = decodeURIComponent(hash)
+      if (hash !== decoded) {
+        document.querySelector(decoded).scrollIntoView()
+      }
     }
   }
 }
