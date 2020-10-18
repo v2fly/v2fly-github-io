ba99e0e474828dee58345fd50742b6a1fcfc2022
Kid <44045911+kidonng@users.noreply.github.com>
2020-10-19 00:07:31 +0800

    Oops

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -6,7 +6,9 @@ export default () => {
   // Auto scroll to hash
   document.onreadystatechange = () => {
     if (document.readyState === 'complete') {
-      document.querySelector(decodeURIComponent(location.hash)).scrollIntoView()
+      if (location.hash) {
+        document.querySelector(decodeURIComponent(location.hash)).scrollIntoView()
+      }
     }
   }
 }
