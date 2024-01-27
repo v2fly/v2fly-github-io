74a1c8ac1b3a3317c978a02051dd94acd79157b1
Nicholas Wang <7414442+nicholascw@users.noreply.github.com>
2024-01-27 11:29:26 -0600

    provide separate entry for v4 and v5 configuration docs (#440)
    * provide separate entry for v4 and v5 configuration docs
    
    * eng ver
    
    ---------
    
    Co-authored-by: V2FlyContrib <SharedAccount+V2FlyContrib@unial.org>

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -57,7 +57,8 @@ export default defineUserConfig({
                 contributorsText: '贡献者',
                 navbar: [
                     { text: '快速开始', link: '/guide/start', },
-                    { text: '配置文档', link: '/v5/config/overview', },
+                    { text: '配置文档', link: '/config/overview', },
+                    { text: '配置文档 (v5, WIP)', link: '/v5/config/overview', },
                     { text: '工具列表', link: '/awesome/tools' },
                     { text: '开发手册', link: '/developer/intro/compile', },
                     { text: '新白话文指南', link: 'https://guide.v2fly.org/' },
@@ -223,7 +224,8 @@ export default defineUserConfig({
                 lastUpdatedText: 'Last Updated',
                 navbar: [
                     { text: 'Quick Start', link: '/en_US/guide/start', },
-                    { text: 'Config Reference', link: '/en_US/v5/config/overview', },
+                    { text: 'Config Reference', link: '/en_US/config/overview', },
+                    { text: 'Config Reference (v5, WIP)', link: '/en_US/v5/config/overview', },
                     { text: 'Tools', link: '/en_US/awesome/tools' },
                     { text: 'Developer Guide', link: '/en_US/developer/intro/compile', },
                     { text: 'New concise guide', link: 'https://guide.v2fly.org/' },
